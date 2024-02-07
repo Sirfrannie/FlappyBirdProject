61e0447f78a4ee340b33bbc79e423d0506941ca2
Sirfrannie <kittikun.jaroomklue@gmail.com>
2024-02-07 21:30:08 +0700

    test me

--- game/Flappybird.java
+++ game/Flappybird.java
@@ -1,20 +1,20 @@
 import javax.swing.*;
+import java.awt.Dimension;
 
 public class Flappybird {
-    public static Flappybird flappyBird;
-
     public Flappybird() {
         JFrame jframe = new JFrame();
         Panel panel = new Panel();
 
         jframe.add(panel);
         jframe.setTitle("FlappyBird");
-        jframe.setSize(1280, 720);
+        jframe.setSize(new Dimension(1280, 720));
         jframe.setVisible(true);
+        jframe.setResizable(false);
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     public static void main(String[] args) {
-        flappyBird = new Flappybird();
+        new Flappybird();
     }
 }
